3a5decab808cd07e3d607e2eb538abec486f16ab
iiakash03 <105138922+iiakash03@users.noreply.github.com>
2024-01-30 13:36:02 +0530

    recent commit

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -8,7 +8,7 @@ const NavBar = () => {
     setNav(!nav)
   }
   return (
-    <div className='fixed w-full h-[80px] flex justify-between item-center px-4 bg-black text-gray-300'>
+    <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300'>
       <div>
         <img src={logo} alt="" style={{width:'75px'}}></img>
       </div>
