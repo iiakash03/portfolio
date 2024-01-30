79ccce21df9a4eb9f0ba44bbd03c9374cfe0edeb
iiakash03 <105138922+iiakash03@users.noreply.github.com>
2024-01-30 13:19:29 +0530

    portfolio navBar

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,45 @@
+import React,{useState} from 'react'
+import {FaBars,FaTimes} from 'react-icons/fa'
+import logo from '../assets/logo-color.png'
+
+const NavBar = () => {
+  const [nav,setNav] = useState(false)
+  const handleClick=()=>{
+    setNav(!nav)
+  }
+  return (
+    <div className='fixed w-full h-[80px] flex justify-between item-center px-4 bg-black text-gray-300'>
+      <div>
+        <img src={logo} alt="" style={{width:'75px'}}></img>
+      </div>
+    
+        <ul className='hidden md:flex'>
+        <li>Home</li>
+        <li>About</li>
+        <li>Work</li>
+        <li>Contact</li>
+        </ul>
+      
+
+      {/*Hamburgur*/}
+
+      <div onClick={handleClick} className='md:hidden z-10'>
+        {!nav?<FaBars/>:<FaTimes/>}
+      </div>
+
+      {/*mobile menu*/}
+
+      <ul className={!nav?'hidden':'absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center'}>
+        <li className='py-6 text-4xl'>Home</li>
+        <li className='py-6 text-4xl'>About</li>
+        <li className='py-6 text-4xl'>Work</li>
+        <li className='py-6 text-4xl'>Contact</li>
+      </ul>
+
+
+      
+    </div>
+  )
+}
+
+export default NavBar
