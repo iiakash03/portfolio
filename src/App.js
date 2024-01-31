cd0993785bb43a60ce0641471c189d3b0243b001
iiakash03 <105138922+iiakash03@users.noreply.github.com>
2024-01-31 13:15:34 +0530

    added about

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
 import NavBar from './components/NavBar';
+import Home from './components/Home';
+import About from './components/About';
 
 function App() {
   return (
     <div className="App">
-      <NavBar>
-        
-      </NavBar>
+      <NavBar/>
+      <Home/>
+      <About/>
     </div>
   );
 }
